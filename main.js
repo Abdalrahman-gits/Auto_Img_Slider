533cc4b524905f988d7313c0ad767441227f880e
Abdalrahman_Gamal <ghozalabdo456@gmail.com>
2025-04-30 02:13:55 +0300

    Adding Comments

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
+// get Elements 
 let sliderIndicators = Array.from(document.querySelectorAll(".image-slider .inner-indicators .indicator-item"));
 let sliderImages = Array.from(document.querySelectorAll(".image-slider .inner-images .image-item"));
 let prevBtn = document.querySelector(".image-slider .prev");
 let nextBtn = document.querySelector(".image-slider .next");
+
+// image index
 let imgIndex = 0;
+
+// Id which Stops the Auto Sliding
 let autoSlideId;
 
+
+
+
+// Find if active class found in html elements
 function ifActiveFound(sliderImages) {
   return sliderImages.some(img => img.classList.contains("active"));
 }
 
+// Removes active Class 
 function removeActive(items) {
   items.forEach((item) => {
     item.classList.remove("active");
   })
 }
 
+
+// Set the Active Image
 function setImage(index) {
   removeActive(sliderImages);
   removeActive(sliderIndicators);
@@ -23,6 +35,7 @@ function setImage(index) {
   sliderIndicators[index].classList.add("active");
 }
 
+// Moves Index to the Next Page
 function nextPage() {
   if(+imgIndex === sliderImages.length-1){
     imgIndex = 0;
@@ -34,6 +47,8 @@ function nextPage() {
   setImage(imgIndex);
 }
 
+
+// Moves index to the Previous Page
 function prevPage() {
   if(!(+imgIndex)){
     imgIndex = sliderImages.length-1;
@@ -45,6 +60,8 @@ function prevPage() {
   setImage(imgIndex);
 }
 
+
+// Auto Sliding Function
 function autosliding() {
   stopsliding();
   autoSlideId = setInterval(() => {
@@ -53,17 +70,21 @@ function autosliding() {
   }, 3000);
 }
 
+
+// Stop Auto Sliding Function
 function stopsliding() {
   clearInterval(autoSlideId);
 }
 
+
+// Procceding the Auto Sliding After delay
 function resumeAfterDelay() {
   setTimeout(() => {
     autosliding();
   }, 3000);
 }
 
-
+// if No active class found => it Initiates.
 if(!ifActiveFound(sliderImages)){
   sliderImages[0].classList.add("active");
   sliderIndicators[0].classList.add("active");
@@ -76,6 +97,7 @@ else {
   })
 }
 
+// Handling Slides Indicators
 sliderIndicators.forEach((indicator) => {
   indicator.addEventListener("click",() => {
     imgIndex = indicator.getAttribute("data-index");
@@ -83,9 +105,13 @@ sliderIndicators.forEach((indicator) => {
   })
 });
 
+
+// Events For Next,Prev Buttons
 prevBtn.addEventListener("click", prevPage);
 nextBtn.addEventListener("click", nextPage);
 
+
+// Events For Next,Prev Using Keyboard
 document.addEventListener("keydown",(event) => {
   if(event.key === "ArrowLeft" || event.key === "ArrowDown"){
     prevPage();
@@ -97,10 +123,13 @@ document.addEventListener("keydown",(event) => {
     stopsliding();
     resumeAfterDelay();
   }
+});
 
-})
 
+// Events For mouse entering || leaving
 document.querySelector(".image-slider").addEventListener("mouseenter", stopsliding);
 document.querySelector(".image-slider").addEventListener("mouseleave", autosliding);
 
+
+// Initial call for Auto Sliding Function
 autosliding();
